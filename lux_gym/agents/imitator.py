98be1981c8f965c26a29e15ea1c0d60cc8806111
Limash <yakubov.sha@gmail.com>
2021-09-09 10:40:42 +0200

    Change to categorical sampling in the imitator agent

--- lux_gym/agents/imitator.py
+++ lux_gym/agents/imitator.py
@@ -10,7 +10,7 @@ import lux_gym.envs.tools as env_tools
 def get_policy():
     feature_maps_shape = tools.get_feature_maps_shape('lux_gym:lux-v0')
     actions_shape = len(action_vector)
-    model = models.get_actor_critic()
+    model = models.actor_critic_1()
     dummy_input = (tf.ones(feature_maps_shape, dtype=tf.float32),
                    tf.convert_to_tensor(worker_action_mask, dtype=tf.float32))
     dummy_input = tf.nest.map_structure(lambda x: tf.expand_dims(x, axis=0), dummy_input)
@@ -56,8 +56,8 @@ def get_policy():
             action, value = predict(obs, worker_action_mask_exp)
             action_masked = action * worker_action_mask_exp
             workers_actions_probs_dict[key] = action_masked[0].numpy()
-            max_arg = tf.argmax(action_masked, axis=-1)[0]
-            # max_arg = tf.squeeze(tf.random.categorical(tf.math.log(action_masked), 1))
+            # max_arg = tf.argmax(action_masked, axis=-1)[0]
+            max_arg = tf.squeeze(tf.random.categorical(tf.math.log(action_masked), 1))
             action_one_hot = tf.one_hot(max_arg, actions_shape)
             workers_actions_dict[key] = action_one_hot[0].numpy()
 
@@ -87,8 +87,8 @@ def get_policy():
             action, value = predict(obs, citytile_action_mask_exp)
             action_masked = action * citytile_action_mask_exp
             citytiles_actions_probs_dict[key] = action_masked[0].numpy()
-            max_arg = tf.argmax(action_masked, axis=-1)[0]
-            # max_arg = tf.squeeze(tf.random.categorical(tf.math.log(action_masked), 1))
+            # max_arg = tf.argmax(action_masked, axis=-1)[0]
+            max_arg = tf.squeeze(tf.random.categorical(tf.math.log(action_masked), 1))
             action_one_hot = tf.one_hot(max_arg, actions_shape)
             citytiles_actions_dict[key] = action_one_hot[0].numpy()
 
