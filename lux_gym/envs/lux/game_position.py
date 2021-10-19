0739f34c1c9791d142fc2945e906c0734bc2878e
Shamil Yakubov <yakubov.sha@gmail.com>
2021-10-19 14:49:35 +0200

    Add new actions vector file and update game_position for new actions represantation

--- lux_gym/envs/lux/game_position.py
+++ lux_gym/envs/lux/game_position.py
@@ -42,6 +42,26 @@ class Position:
         elif direction == DIRECTIONS.CENTER:
             return Position(self.x, self.y)
 
+    def direction_to(self, target_pos: 'Position') -> DIRECTIONS:
+        """
+        Return closest position to target_pos from this position
+        """
+        check_dirs = [
+            DIRECTIONS.NORTH,
+            DIRECTIONS.EAST,
+            DIRECTIONS.SOUTH,
+            DIRECTIONS.WEST,
+        ]
+        closest_dist = self.distance_to(target_pos)
+        closest_dir = DIRECTIONS.CENTER
+        for direction in check_dirs:
+            newpos = self.translate(direction, 1)
+            dist = target_pos.distance_to(newpos)
+            if dist < closest_dist:
+                closest_dir = direction
+                closest_dist = dist
+        return closest_dir
+
     def __str__(self) -> str:
         return f"({self.x}, {self.y})"
 
